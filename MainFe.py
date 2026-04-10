c22da87d673fed153dc6b00a537a419437619f3b
ANORA\vishnuvarshini.r <anora>
2026-04-10 10:40:49 +0530

    fix in input

--- MainFe.py
+++ MainFe.py
@@ -2,6 +2,11 @@ print("Hello This is my main application")
 
 print("This is calculator")
 
-num1= int(input("Enter num1: "))
-num2=int(input("Enter num2: "))
-print(num1, num2)
+num1= input("Enter num1: ")
+num2=input("Enter num2: ")
+if not num1.isnumeric() or not num2.isnumeric():
+    print("enter correctly")
+
+n1=int(num1)
+n2=int(num2)
+
